5766cae57b5b94726edcd4fa7e447623f35d46e4
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-08-30 14:10:13 -0500

    Const removed & index updated
    Index updated with h1 and h2 tags. Const. removed to make first sentence easier.

--- mods.js
+++ mods.js
@@ -7,16 +7,10 @@ function toUpper(text) {
 
 toUpper(myself) ; // I know this is a funtion pulled from the video but it was meant for testing purposes
 
-const car = {
-    carName: 'Bel-Air' ,
-    carEngine: '210' ,
-    carColor: 'Copper' ,
-    carDate: '1962' ,
-}
 
 function carYear(y1, y2) {
     return y1 * y2 ;
 }
 
 let result = carYear(489, 4);
-document.getElementById("bel").innerHTML = "My grandfather owned a" + " " + result + " " + car + " " + carName ;
+document.getElementById("bel").innerHTML = "My grandfather owned a" + " " + result + " " + "Chevy Bel-Air."
