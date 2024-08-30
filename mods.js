653168d867c9c0a5887c1a4deadfcc84240c064a
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-08-30 14:27:46 -0500

    Const. re-established
    Const. was re-established with object methods & another function.

--- mods.js
+++ mods.js
@@ -14,3 +14,14 @@ function carYear(y1, y2) {
 
 let result = carYear(489, 4);
 document.getElementById("bel").innerHTML = "My grandfather owned a" + " " + result + " " + "Chevy Bel-Air."
+
+const seller = {
+    firstName: 'Some' ,
+    lastName: 'Guy' ,
+    date: 1962 ,
+    fullSeller: function() {
+        return this.firstName + " " + this.lastName + " " + "in" + " " + this.date ;
+    }
+} ;
+
+document.getElementById("sell").innerHTML = seller.fullSeller() ;
